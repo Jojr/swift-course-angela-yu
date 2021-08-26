aebfaa7f32bde5936209b42cc09f5e755b8b9acf
João Belem Junior <>
2021-08-26 09:00:31 -0300

    Update to lesson 11

---
+++ I Am Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by João Belem Junior on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
